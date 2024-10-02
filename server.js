4462b4baafd7f2357ab3ebe164af5ad023598e9f
pauval <pauly.val@gmail.com>
2024-10-01 21:51:02 -0300

    rev

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.post('/login', async (req, res) => {
     }
 });
 
-function authenticateToken(req, res, next) {
+/* function authenticateToken(req, res, next) {
     const token = req.headers['authorization'];
     if (!token) return res.sendStatus(401);
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
@@ -59,7 +59,22 @@ function authenticateToken(req, res, next) {
         req.user = user;
         next();
     });
+} */
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    
+    const token = authHeader && authHeader.split(' ')[1];
+    
+    if (!token) return res.sendStatus(401); 
+    
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403); 
+        req.user = user; 
+        next(); 
+    });
 }
+
 
 app.get('/usuarios', authenticateToken, async (req, res) => {
     try {
